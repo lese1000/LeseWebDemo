4e7dd3f5bc1b3535fe4ded367ce4e63fb7e269f7
lese1000 <598936703@qq.com>
2017-04-20 18:11:59 +0800

    增加获取ip的工具类

---
+++ src/main/java/tech/lese/webdemo/utils/IpUtils.java
@@ -0,0 +1,27 @@
+package tech.lese.webdemo.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+//http://blog.csdn.net/songylwq/article/details/7701718
+public class IpUtils {  
+    public static String getIpAddr(HttpServletRequest request) {  
+        String ip = request.getHeader("X-Forwarded-For");  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("WL-Proxy-Client-IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_CLIENT_IP");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+        }  
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+            ip = request.getRemoteAddr();  
+        }  
+        return ip;  
+    }  
+} 
+
